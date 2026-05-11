b317c7415dd3cdc387908f9e9c5de3f740e078a2
malwshechka <valeriayurtsevich2008@gmail.com>
2026-05-11 19:47:54 +0300

    Setup for Render with ENV variables

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.HttpOverrides;
 using System.Globalization;
 using WebApplication4;
 using WebApplication4.Data;
@@ -12,17 +12,11 @@ using WebApplication4.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// DATABASE
-       var connectionString =
-    builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString, o =>
-    {
-        o.CommandTimeout(300);
-    }));
+    options.UseNpgsql(connectionString, o => { o.CommandTimeout(300); }));
 
-// IDENTITY
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
@@ -32,126 +26,74 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     options.Password.RequiredLength = 6;
 
     options.SignIn.RequireConfirmedEmail = false;
-
     options.User.RequireUniqueEmail = true;
 })
 .AddEntityFrameworkStores<AppDbContext>()
 .AddDefaultTokenProviders();
 
-// COOKIE
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Identity/Account/Login";
     options.LogoutPath = "/Identity/Account/Logout";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-
     options.Cookie.HttpOnly = true;
-
-    // ВАЖНО ДЛЯ OAUTH
     options.Cookie.SameSite = SameSiteMode.None;
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-
     options.ExpireTimeSpan = TimeSpan.FromDays(30);
-
     options.SlidingExpiration = true;
 });
 
-// AUTH
 builder.Services
     .AddAuthentication(options =>
     {
-        options.DefaultScheme =
-            IdentityConstants.ApplicationScheme;
-
-        options.DefaultAuthenticateScheme =
-            IdentityConstants.ApplicationScheme;
-
-        options.DefaultChallengeScheme =
-            IdentityConstants.ApplicationScheme;
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
     })
-
-    // GOOGLE
     .AddGoogle(options =>
     {
-        options.ClientId =
-            builder.Configuration["Authentication:Google:ClientId"]!;
-
-        options.ClientSecret =
-            builder.Configuration["Authentication:Google:ClientSecret"]!;
-
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
         options.CallbackPath = "/signin-google";
-
         options.SaveTokens = true;
     })
-
-    // GITHUB
     .AddGitHub(options =>
     {
-        options.ClientId =
-            builder.Configuration["Authentication:GitHub:ClientId"]!;
-
-        options.ClientSecret =
-            builder.Configuration["Authentication:GitHub:ClientSecret"]!;
-
+        options.ClientId = builder.Configuration["Authentication:GitHub:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"]!;
         options.CallbackPath = "/signin-github";
-
         options.Scope.Add("user:email");
-
         options.SaveTokens = true;
     })
-
-    // DISCORD
     .AddDiscord(options =>
     {
-        options.ClientId =
-            builder.Configuration["Authentication:Discord:ClientId"]!;
-
-        options.ClientSecret =
-            builder.Configuration["Authentication:Discord:ClientSecret"]!;
-
+        options.ClientId = builder.Configuration["Authentication:Discord:ClientId"]!;
+        options.ClientSecret = builder.Configuration["Authentication:Discord:ClientSecret"]!;
         options.CallbackPath = "/signin-discord";
-
         options.Scope.Add("identify");
         options.Scope.Add("email");
-
         options.SaveTokens = true;
     });
 
-// EMAIL
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 
-// LOCALIZATION
-builder.Services.AddLocalization(options =>
-    options.ResourcesPath = "Resources");
-
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-    var supportedCultures = new[]
-    {
-        new CultureInfo("ru-RU"),
-        new CultureInfo("en-US")
-    };
-
-    options.DefaultRequestCulture =
-        new RequestCulture("ru-RU");
-
-    options.SupportedCultures =
-        supportedCultures;
-
-    options.SupportedUICultures =
-        supportedCultures;
-
-    options.RequestCultureProviders.Insert(
-        0,
-        new QueryStringRequestCultureProvider());
+    var supportedCultures = new[] { new CultureInfo("ru-RU"), new CultureInfo("en-US") };
+    options.DefaultRequestCulture = new RequestCulture("ru-RU");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
 });
 
 builder.Services.AddControllersWithViews()
-    .AddViewLocalization(
-        LanguageViewLocationExpanderFormat.Suffix)
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
     .AddDataAnnotationsLocalization();
 
 builder.Services.AddRazorPages();
+
+builder.WebHost.UseUrls("http://0.0.0.0:" + (Environment.GetEnvironmentVariable("PORT") ?? "10000"));
 
 var app = builder.Build();
 
@@ -160,26 +102,18 @@ app.UseRequestLocalization();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-
     app.UseHsts();
 }
 
-// ВАЖНО ДЛЯ RENDER
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
-    ForwardedHeaders =
-        Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor |
-        Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -188,24 +122,20 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-// AUTO MIGRATIONS
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
     try
     {
-        var context =
-            services.GetRequiredService<AppDbContext>();
-
+        var context = services.GetRequiredService<AppDbContext>();
         await context.Database.MigrateAsync();
-
-        //await DbInitializer.Initialize(services);
+        Console.WriteLine("MIGRATIONS APPLIED SUCCESSFULLY");
     }
     catch (Exception ex)
     {
+        Console.WriteLine("MIGRATION ERROR:");
         Console.WriteLine(ex.ToString());
-        throw;
     }
 }
 
